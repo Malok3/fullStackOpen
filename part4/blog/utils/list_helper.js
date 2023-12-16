5edca5c9e7bf7b6dbcde970c58e095350788da1b
Malok3 <niko987@gmail.com>
2023-12-16 22:38:30 +0200

    update

---
+++ part4/blog/utils/list_helper.js
@@ -0,0 +1,17 @@
+const dummy = (blogs) => {
+// ...
+}
+
+const listHelper = require('../utils/list_helper')
+
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
+
+
+module.exports = {
+    dummy
+}
