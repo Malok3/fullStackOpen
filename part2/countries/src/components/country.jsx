ddf8992394b9560d6c5a13901b3dca66df41223f
malok3 <niko987@gmail.com>
2024-01-17 18:32:40 +0200

    countries update

--- part2/countries/src/components/country.jsx
+++ part2/countries/src/components/country.jsx
@@ -1,28 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Country = ({country}) => {
-  const [weather, setWeather] = useState([])
+const Country = ({ country }) => {
+  const [weather, setWeather] = useState({});
+  //const [loading, setLoading] = useState(true);
 
-  axios.get('http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}', {params})
+  console.log(country)
+  useEffect(() => {
+    const apiKey = '836a30f7ac09f6f85c4d73311ccd5626';
+    const lat = country.latlng[0]
+    const lon = country.latlng[1]
+    // get coordinates from country's capital
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    
+      .then(weatherResponse => {
+        setWeather(weatherResponse.data);
+        console.log(weatherResponse)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      })
+      
+  }); // Ajout de country.capital comme dépendance pour useEffect
 
-
-  //Object.keys(country.languages) is an array 
   return (
     <div>
       <h1>{country.name.common}</h1>
-      <p>capital: {country.capital}</p>
-      <p>population: {country.population}</p>
+      <img src={country.flags.png} alt="Flag" />
+      <p>Capital: {country.capital}</p>
+      <p>Population: {country.population}</p>
       <h2>Spoken languages</h2>
       <ul>
-       {Object.keys(country.languages).map((key) => (
-            <li key={key}>{country.languages[key]}</li>
-          ))
-        }              
-      </ul> 
-      <h2>Flag</h2>
-      <img src={country.flags.png} />
+        {Object.values(country.languages).map((language, index) => (
+          <li key={index}>{language}</li>
+        ))}
+      </ul>
+      <h2>Current weather</h2>
+      
+      
+          <p>Temperature: {weather.main.temp} °C</p>
+          <p>Description: {weather.weather[0].description}</p>
+      
+      
     </div>
-  )
-}
+  );
+};
 
-export default Country
+export default Country;
