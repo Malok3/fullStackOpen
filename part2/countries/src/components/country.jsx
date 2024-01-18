fa3bc2c5b5102331c8d62e1f1205d7ce7088d05e
malok3 <niko987@gmail.com>
2024-01-18 17:41:13 +0200

    update country

--- part2/countries/src/components/country.jsx
+++ part2/countries/src/components/country.jsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
+
+//const [loading, setLoading] = useState(true);
+
+const apiKey = import.meta.env.VITE_SOME_KEY
+
 const Country = ({ country }) => {
+  const selectedCountry = props.country
   const [weather, setWeather] = useState({});
   //const [loading, setLoading] = useState(true);
 
   console.log(country)
   useEffect(() => {
-    const apiKey = '836a30f7ac09f6f85c4d73311ccd5626';
-    const lat = country.latlng[0]
-    const lon = country.latlng[1]
+
+    const lat = selectedCountry.latlng[0]
+    const lon = selectedCountry.latlng[1]
+   
     // get coordinates from country's capital
     axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
     
@@ -23,24 +31,26 @@ const Country = ({ country }) => {
       
   }); // Ajout de country.capital comme dépendance pour useEffect
 
+      
+
   return (
     <div>
-      <h1>{country.name.common}</h1>
-      <img src={country.flags.png} alt="Flag" />
-      <p>Capital: {country.capital}</p>
-      <p>Population: {country.population}</p>
+      <h1>{selectedCountry.name.common}</h1>
+      <img src={selectedCountry.flags.png} alt="Flag" />
+      <p>Capital: {selectedCountry.capital}</p>
+      <p>Population: {selectedCountry.population}</p>
       <h2>Spoken languages</h2>
       <ul>
-        {Object.values(country.languages).map((language, index) => (
+        {Object.values(selectedCountry.languages).map((language, index) => (
           <li key={index}>{language}</li>
         ))}
       </ul>
       <h2>Current weather</h2>
       
-      
+{/*       
           <p>Temperature: {weather.main.temp} °C</p>
           <p>Description: {weather.weather[0].description}</p>
-      
+       */}
       
     </div>
   );
