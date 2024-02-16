5acbdeb37a1cf423206bceda667db9e591ab6cd8
malok3 <niko987@gmail.com>
2024-02-16 12:00:23 +0200

    5.14 complete

--- part5/bloglist-frontend/src/components/blog.test.js
+++ part5/bloglist-frontend/src/components/blog.test.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
+import Togglable from './Togglable'
 
 test('renders content', () => {
   const blog = {
@@ -10,9 +12,6 @@ test('renders content', () => {
     url:'http://www.url.com',
     likes:'999'
   }
-
-  
-
   const { container } = render(<Blog blog={blog} />)
 
   const spanAuthor = container.querySelector('.blogTitle')
@@ -23,4 +22,29 @@ test('renders content', () => {
   expect(spanTitle).toHaveTextContent(
     'John Doe'
   )
+})
+
+
+describe('<Togglable />', () => {
+  let container
+
+  beforeEach(() => {
+    container = render(
+      <Togglable buttonLabel="show...">
+        <div className="testDiv" >
+            togglable content
+        </div>
+      </Togglable>
+    ).container
+  })
+
+  test('after clicking the button, children are displayed', async () => {
+    const user = userEvent.setup()
+    console.log(user)
+    const button = screen.getByText('show...')
+    await user.click(button)
+
+    const div = container.querySelector('.togglableContent')
+    expect(div).not.toHaveStyle('display: none')
+  })
 })
