091a45b0d213dbe1f2914b9994589a0b5d0cf9f7
malok3 <niko987@gmail.com>
2024-02-16 12:46:07 +0200

    5.15 complete

--- part5/bloglist-frontend/src/components/blog.test.js
+++ part5/bloglist-frontend/src/components/blog.test.js
@@ -5,27 +5,30 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 import Togglable from './Togglable'
 
-test('renders content', () => {
-  const blog = {
-    title: 'TestBlog name',
-    author: 'John Doe',
-    url:'http://www.url.com',
-    likes:'999'
-  }
-  const { container } = render(<Blog blog={blog} />)
+describe('Blog component', () => {
 
-  const spanAuthor = container.querySelector('.blogTitle')
-  expect(spanAuthor).toHaveTextContent(
-    'TestBlog name'
-  )
-  const spanTitle = container.querySelector('.blogAuthor')
-  expect(spanTitle).toHaveTextContent(
-    'John Doe'
-  )
+  test('Title and author are rendered', () => {
+    const blog = {
+      title: 'TestBlog name',
+      author: 'John Doe',
+      url:'http://www.url.com',
+      likes:'999'
+    }
+    const { container } = render(<Blog blog={blog} />)
+
+    const spanAuthor = container.querySelector('.blogTitle')
+    expect(spanAuthor).toHaveTextContent(
+      'TestBlog name'
+    )
+    const spanTitle = container.querySelector('.blogAuthor')
+    expect(spanTitle).toHaveTextContent(
+      'John Doe'
+    )
+  })
 })
 
 
-describe('<Togglable />', () => {
+describe('Togglable components', () => {
   let container
 
   beforeEach(() => {
@@ -40,11 +43,32 @@ describe('<Togglable />', () => {
 
   test('after clicking the button, children are displayed', async () => {
     const user = userEvent.setup()
-    console.log(user)
     const button = screen.getByText('show...')
     await user.click(button)
 
     const div = container.querySelector('.togglableContent')
     expect(div).not.toHaveStyle('display: none')
   })
+
+  test('if the like button is clicked twice, the component received as props is called twice.', async () => {
+    const blog = {
+      title: 'TestBlog name',
+      author: 'John Doe',
+      url:'http://www.url.com',
+      likes:'0'
+    }
+    // The event handler is a mock function defined with Jest
+    const mockHandler = jest.fn()
+
+    render(
+      <Blog blog={blog} updateBlog={mockHandler}/>
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('Like')
+
+    await user.dblClick(button)
+    expect(mockHandler.mock.calls).toHaveLength(2)
+  })
 })
+
