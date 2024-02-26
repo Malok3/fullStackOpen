1aecf3bb8f66015dea3c2019cd516e0fafb52233
malok3 <niko987@gmail.com>
2024-02-26 21:23:06 +0200

    5.22

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,8 +1,7 @@
 
-describe('Blog app', function() {
-  beforeEach(function() {
+describe('Blog app', function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:5173/api/testing/reset')
-
     const user = {
       name: 'Nicolas',
       username: 'testing_user',
@@ -12,12 +11,12 @@ describe('Blog app', function() {
     cy.visit('http://localhost:5173')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.contains('Login to application')
   })
 
-  describe('Login',function() {
-    it('succeeds with correct credentials', function() {
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
       cy.contains('login').click()
       cy.get('#username').type('testing_user')
       cy.get('#password').type('123456')
@@ -25,23 +24,23 @@ describe('Blog app', function() {
       cy.contains('Nicolas logged in')
     })
 
-    it('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function () {
       cy.contains('login').click()
       cy.get('#username').type('testing_user')
       cy.get('#password').type('wrong')
       cy.get('#login-button').click()
-  
+
       cy.contains('wrong credentials')
       cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
     })
   })
 
-  describe('When logged in', function() {
-    beforeEach(function() {
+  describe('When logged in', function () {
+    beforeEach(function () {
       cy.login({ username: 'testing_user', password: '123456' })
     })
 
-    it('A blog can be created', function() {
+    it('A blog can be created', function () {
       cy.get('#create-newblog-button').click()
       cy.get('#blog-title').type('Le bateau ivre')
       cy.get('#blog-author').type('Arthur Rimbaud')
@@ -50,7 +49,7 @@ describe('Blog app', function() {
       cy.contains('Arthur Rimbaud')
     })
 
-    it('A blog can be liked', function(){
+    it('A blog can be liked', function () {
       cy.createBlog({
         title: 'another blog cypress',
         author: 'tester',
@@ -62,18 +61,45 @@ describe('Blog app', function() {
       cy.get('#like-button').click()
       cy.contains('1')
     })
-    it.only('A blog can be deleted', function(){
+
+
+  })
+
+  describe('Deleting a blog', function () {
+    beforeEach(function () {
+      cy.login({ username: 'testing_user', password: '123456' })
       cy.createBlog({
         title: 'a blog to delete',
         author: 'tester',
         url: 'testing'
       })
       cy.contains('a blog to delete')
+    })
+
+    it('A blog can be deleted', function () {
       cy.contains('a blog to delete').parent().find('#seemore-button').click()
       cy.get('#delete-button').as('theButton')
       cy.get('@theButton').click()
-      cy.get('@theButton').should('not.exist')
+      cy.contains('a blog to delete').should('not.exist')
     })
+
+    it.only('only the creator can see the delete button', function () {
+      const user = {
+        name: 'john',
+        username: 'johndoe',
+        password: '123456'
+      }
+      cy.request('POST', 'http://localhost:5173/api/users', user)
+      cy.visit('http://localhost:5173')
+      cy.login({ username: 'johndoe', password: '123456' })
+      cy.contains('a blog to delete').parent().find('#seemore-button').click()
+      cy.get('#delete-button').click()
+      cy.contains('Error in deleting blog')
+      cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+
   })
 
 })
+
+
