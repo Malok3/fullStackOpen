915a9627d0d0a9c3be7504ca5f7cc9400f2b2f33
malok3 <niko987@gmail.com>
2024-02-26 18:20:38 +0200

    5.19 complete

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -16,6 +16,7 @@ describe('Blog app', function() {
   it('Login form is shown', function() {
     cy.contains('Login to application')
   })
+
   describe('Login',function() {
     it('succeeds with correct credentials', function() {
       cy.contains('login').click()
@@ -35,4 +36,24 @@ describe('Blog app', function() {
       cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
     })
   })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      cy.contains('login').click()
+      cy.get('#username').type('testing_user')
+      cy.get('#password').type('123456')
+      cy.get('#login-button').click()
+      cy.contains('Nicolas logged in')
+    })
+
+    it('A blog can be created', function() {
+      cy.get('#create-newblog-button').click()
+      cy.get('#blog-title').type('Le bateau ivre')
+      cy.get('#blog-author').type('Arthur Rimbaud')
+      cy.get('#blog-url').type('http://www.test.fi')
+      cy.get('#blog-submit').click()
+      cy.contains('Arthur Rimbaud')
+    })
+  })
+
 })
