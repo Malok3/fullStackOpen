d979c03d660f7cd52447790bb58e555ceaaf7adb
malok3 <niko987@gmail.com>
2024-03-12 11:00:27 +0200

    fix

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,9 +1,9 @@
-import { createStore } from 'redux'
+//import { createStore } from 'redux'
 import {AnecdoteForm} from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
-import { reducer } from './reducers/anecdoteReducer'
+//import { reducer } from './reducers/anecdoteReducer'
 
-const store = createStore(reducer) //creates redux store with reducer as argument. App state will be saved in store
+//const store = createStore(reducer) //creates redux store with reducer as argument. App state will be saved in store
 
 const App = () => {
 
